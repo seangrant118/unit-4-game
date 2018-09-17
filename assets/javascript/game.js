791f0d3817cdeb9a36137f7b7b3d8d3d1d24d399
Sean Grant <seangrant118@gmail.com>
2018-09-17 18:18:01 -0400

    functional game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,32 +20,141 @@ $(document).ready(function () {
   $("#crystal-1").on("click", function () {
     totalScore = totalScore + crystalOneValue;
     $("#total-score").text(totalScore);
+    if (totalScore === randomNumber) {
+      wins++;
+      $("#win-total").text("Wins: " + wins);
+      console.log(wins);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    } else if (totalScore > randomNumber) {
+      losses++;
+      $("#loss-total").text("Losses: " + losses);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    }
   });
 
   $("#crystal-2").on("click", function () {
     totalScore = totalScore + crystalTwoValue;
     $("#total-score").text(totalScore);
+    if (totalScore === randomNumber) {
+      wins++;
+      $("#win-total").text("Wins: " + wins);
+      console.log(wins);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    } else if (totalScore > randomNumber) {
+      losses++;
+      $("#loss-total").text("Losses: " + losses);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    }
   });
   $("#crystal-3").on("click", function () {
     totalScore += crystalThreeValue;
     $("#total-score").text(totalScore);
+    if (totalScore === randomNumber) {
+      wins++;
+      $("#win-total").text("Wins: " + wins);
+      console.log(wins);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    } else if (totalScore > randomNumber) {
+      losses++;
+      $("#loss-total").text("Losses: " + losses);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    }
   });
   $("#crystal-4").on("click", function () {
     totalScore += crystalFourValue;
     $("#total-score").text(totalScore);
+    if (totalScore === randomNumber) {
+      wins++;
+      $("#win-total").text("Wins: " + wins);
+      console.log(wins);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    } else if (totalScore > randomNumber) {
+      losses++;
+      $("#loss-total").text("Losses: " + losses);
+      totalScore = 0;
+      $("#total-score").text(totalScore);
+      crystalOneValue = Math.floor(Math.random() * 12) + 1;
+      crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+      crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+      crystalFourValue = Math.floor(Math.random() * 12) + 1;
+      randomNumber = Math.floor(Math.random() * 120) + 19;
+      $("#random-number").text("You must match: " + randomNumber);
+    }
   });
 
-  if (totalScore === randomNumber) {
-    wins++;
-    $("#win-total").text("Wins: " + wins);
-    console.log(wins);
-    totalScore = 0;
-    $("#total-score").text(totalScore);
-    crystalOneValue = Math.floor(Math.random() * 12) + 1;
-    crystalTwoValue = Math.floor(Math.random() * 12) + 1;
-    crystalThreeValue = Math.floor(Math.random() * 12) + 1;
-    crystalFourValue = Math.floor(Math.random() * 12) + 1;
-  }
+  // if (totalScore === randomNumber) {
+  //   wins++;
+  //   $("#win-total").text("Wins: " + wins);
+  //   console.log(wins);
+  //   totalScore = 0;
+  //   $("#total-score").text(totalScore);
+  //   crystalOneValue = Math.floor(Math.random() * 12) + 1;
+  //   crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+  //   crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+  //   crystalFourValue = Math.floor(Math.random() * 12) + 1;
+  //   randomNumber = Math.floor(Math.random() * 120) + 19;
+  //   $("#random-number").text("You must match: " + randomNumber);
+  // } else if (totalScore > randomNumber) {
+  //   losses++;
+  //   $("#loss-total").text("Losses: " + losses);
+  //   totalScore = 0;
+  //   $("#total-score").text(totalScore);
+  //   crystalOneValue = Math.floor(Math.random() * 12) + 1;
+  //   crystalTwoValue = Math.floor(Math.random() * 12) + 1;
+  //   crystalThreeValue = Math.floor(Math.random() * 12) + 1;
+  //   crystalFourValue = Math.floor(Math.random() * 12) + 1;
+  //   randomNumber = Math.floor(Math.random() * 120) + 19;
+  //   $("#random-number").text("You must match: " + randomNumber);
+  // }
   // if total score = random number add a win and reset game
   // if total score > random number add a loss and reset game
 })
